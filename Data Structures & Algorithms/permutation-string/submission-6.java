f49cfa31f3a20fc39824c8e377448156cbc9b8b8
joelkishore33 <joelkishore33@gmail.com>
2026-05-28 12:51:38 -0400

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        ArrayList<Character> joel = new ArrayList<>();
+        ArrayList<Character> backupList = new ArrayList<>();
+        ArrayList<Character> jason = new ArrayList<>();
+
+        for (int i = 0; i < s1.length(); i++){
+            joel.add(s1.charAt(i));
+            backupList.add(s1.charAt(i));
+        }
+
+        for (int i = 0; i < s2.length(); i++){
+            jason.add(s2.charAt(i));
+        }
+
+        for (int i = 0; i < jason.size(); i++){
+            if(joel.size() == 0){
+                return true;
+            }
+            if (joel.contains(jason.get(i))){
+                joel.remove(jason.get(i));
+                if (joel.size() == 0) {
+                    return true;
+                }
+            }
+            else if (!(joel.contains(jason.get(i))) && joel.size() < backupList.size()){
+                int x = backupList.size() - joel.size();
+                joel = new ArrayList<>(backupList);
+                i = i - x;
+            }
+        }
+
+        return false;
+
+    }
+}
